db1048e9b7b693b2cafba3e16bd39a8b29cf8160
edilkk <edilmoldokanov@gmail.com>
2020-02-26 14:24:54 -0600

    addded git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("Say Hello"){
-        properties([parameters([string(defaultValue: 'Ed', description: 'Please paste ur name', name: 'NAME', trim: false)]), pipelineTriggers([cron('* * * * *')])])
-        sh "echo Hello ${NAME}"
+        git 'https://github.com/edilkk/jenkins-test.git'
+        
     }
 }
