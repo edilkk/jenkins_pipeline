eeecc9acfb027de124d39828f467763be3d71e5e
edilkk <edilmoldokanov@gmail.com>
2020-02-26 18:19:27 -0600

    dasda

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,8 @@ node {
     stage("Say Hello"){
         properties([parameters([string(defaultValue: '54.187.181.98', description: '''Dev: 54.187.181.98 QA: 54.202.46.240 Prod: 52.41.241.15''', name: 'Remote_instance', trim: false)]), pipelineTriggers([cron('* * * * *')])])
         git 'https://github.com/edilkk/jenkins-test.git'
-
+    }
+    stage("Install Apache"){
+        sh "ssh ec2-user@${Remote_instance}   sudo yum install httpd -y"
     }
 }
